57fc12bf53f47388450cbb94aa3fe40f7d8a9b55
Eugenia-Khaustova <yevheniia.khaustova@gmail.com>
2022-02-15 01:37:36 +0200

    Final commit

--- script.js
+++ script.js
@@ -13,23 +13,169 @@ function searchString(originalStr, str) {
   return result;
 }
 
-console.log(searchString("Everything", "every"))
-console.log(searchString("Everything", "Every"))
+console.log(searchString("Everything", "every"));
+console.log(searchString("Everything", "Every"));
 
-
-// 2. Написать функцию capitalizeFirstLetter , которая будет 
-// принимать строку. Функция возвращает строку, где первый символ 
+// 2. Написать функцию capitalizeFirstLetter , которая будет
+// принимать строку. Функция возвращает строку, где первый символ
 // будет с заглавной буквы
 
-let capResult
-let lowResult
-function capitalizeFirstLetter (str) {
-    let stringArray = str.split("")
-    if(stringArray[0]) {
-        stringArray[0] = stringArray[0].toUpperCase()
-    }
-    stringArray = stringArray.join("")
-    return stringArray
-} 
+let capResult;
+let lowResult;
+function capitalizeFirstLetter(str) {
+  let stringArray = str.split("");
+  if (stringArray[0]) {
+    stringArray[0] = stringArray[0].toUpperCase();
+  }
+  stringArray = stringArray.join("");
+  return stringArray;
+}
 
-console.log(capitalizeFirstLetter("eleven"))
+console.log(capitalizeFirstLetter("hello")); // Hello
+console.log(capitalizeFirstLetter("HELLO")); // HELLO
+console.log(capitalizeFirstLetter("hElLo")); // HElLo
+
+// 3. Создайте функцию truncate(str, maxlength), которая проверяет длину строки
+// str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы
+// её длина стала равна maxlength. Результатом функции должна быть та же строка,
+// если усечение не требуется, либо, если необходимо, усечённая строка.
+
+function truncate(str, maxlength) {
+  if (str.length > maxlength) {
+    return str.substr(0, maxlength) + "...";
+  } else {
+    return str;
+  }
+}
+
+console.log(truncate("Hello world", 5)); // Hello...
+console.log(truncate("Hello world", 25)); // Hello world
+
+// 4. Есть массив студентов. Для реализации используем методы массивов.
+// Написать две функции:
+//     Должны вывести массив студентов и средний бал каждого
+//     Должна вывести средний бал по всем студентам
+// На выходе мы должны получить новый массив (после выполнения первой функции!)
+
+function studentAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].marks.length; j++) {
+      sum += arr[i].marks[j];
+    }
+    arr[i].averageMark = sum / arr[i].marks.length;
+    sum = 0;
+  }
+  return arr;
+}
+
+let averageMarkArr = 0;
+let arrayObjLength = 0;
+function averageMarkArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].marks.length; j++) {
+      sum += arr[i].marks[j];
+    }
+    arrayObjLength += arr[i].marks.length;
+  }
+  return (averageMarkArr = sum / arrayObjLength);
+}
+
+const students = [
+  {
+    name: "John Smith",
+    marks: [10, 8, 6, 9, 8, 7],
+  },
+  {
+    name: "John Doe",
+    marks: [9, 8, 7, 6, 7],
+  },
+  {
+    name: "Thomas Anderson",
+    marks: [6, 7, 10, 8],
+  },
+  {
+    name: "Jean-Baptiste Emanuel Zorg",
+    marks: [10, 9, 8, 9],
+  },
+];
+console.log(studentAverage(students));
+console.log(averageMarkArray(students));
+
+// 5. Данн массив vehicles. Необходимо написать код, который реализует подсчет
+// средней цены автомобилей типа SUV:
+// Обратите внимание, что поле type может быть написано в любом регистре символов (suv, SUV).
+// Примечание: для решения задачи вам нужно будет использовать методы массивов (а не обычные циклы)
+
+let suvPrice = 0;
+let i = 0;
+function suvAveragePrice(arr) {
+  let suvArray = arr.filter((item) => item.type == "suv" || item.type == "SUV");
+  suvArray.forEach((el) => {
+    suvPrice += el.price;
+  });
+  return suvPrice / suvArray.length;
+}
+
+const vehicles = [
+  { make: "Honda", model: "CR-V", type: "suv", price: 24045 },
+  { make: "Honda", model: "Accord", type: "sedan", price: 22455 },
+  { make: "Mazda", model: "Mazda 6", type: "sedan", price: 24195 },
+  { make: "Mazda", model: "CX-9", type: "suv", price: 31520 },
+  { make: "Toyota", model: "4Runner", type: "suv", price: 34210 },
+  { make: "Toyota", model: "Sequoia", type: "SUV", price: 45560 },
+  { make: "Toyota", model: "Tacoma", type: "truck", price: 24320 },
+  { make: "Ford", model: "F-150", type: "truck", price: 27110 },
+  { make: "Ford", model: "Fusion", type: "sedan", price: 22120 },
+  { make: "Ford", model: "Explorer", type: "SUV", price: 31660 },
+];
+
+console.log(suvAveragePrice(vehicles));
+
+// 6. У нас есть массив чисел arr. Нужно получить отсортированную копию, но
+// оставить arr неизменённым. Создайте функцию copySorted(arr), которая будет
+// возвращать такую копию.
+// Примечание: есть несколько вариантов решения этой задачи.
+// Некоторые из них мы еще не разбирали. Можете написать несколько вариантов реализации.
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+function spreadSorted(arr) {
+  let spreadCopyArr = [...arr];
+  return spreadCopyArr.sort();
+}
+
+function arrayFromSorted(arr) {
+  let arrayFromCopyArr = Array.from(arr);
+  return arrayFromCopyArr.sort();
+}
+
+let arr = [10, 20, 3, 4, 0, 99, 97];
+let sorted = copySorted(arr);
+let spreadSortedArr = spreadSorted(arr);
+let arrayFromSortedArr = arrayFromSorted(arr);
+
+console.log(arr); // [10, 20, 3, 4, 0, 99, 97] - массив не должен измениться!
+console.log(sorted); // [0, 3, 4, 10, 20, 97, 99]
+console.log(spreadSortedArr);
+console.log(arrayFromSortedArr);
+
+// 7. Напишите функцию getKeysCount(obj), которая возвращает количество свойств
+// объекта. Постарайтесь сделать код как можно короче.
+// Примечание: идеальное решение может быть в 1 строку
+
+let getKeysCount = (obj) => {
+  return Object.keys(obj).length;
+};
+
+let user = {
+  name: "John",
+  lastname: "Smith",
+  age: "21",
+  city: "London",
+};
+
+console.log(getKeysCount(user))
